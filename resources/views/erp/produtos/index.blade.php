f2db881471bf3505af0d1e1f9f31ec4862c432bf
José Roberto Dias Pereira Junior <dev1@f5sg.com.br>
2025-01-21 17:32:58 -0300

    produtos

--- resources/views/erp/produtos/index.blade.php
+++ resources/views/erp/produtos/index.blade.php
@@ -1,7 +1,50 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+            <div class="col">
+                <div class="card p-2">
+                    <h6 class="text-center">Filtros</h6>
+                </div>
+            </div>
+        </div>
 
+        <div class="row justify-content-center mt-5">
+            <div class="col">
+                <div class="card p-2">
+                    <h6 class="text-center">Produtos</h6>
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th class="table-secondary">Descrição</th>
+                                <th class="table-secondary">Descrição</th>
+                                <th class="table-secondary">Descrição</th>
+                                <th class="table-secondary">Descrição</th>
+                                <th class="table-secondary">Descrição</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>teste 1</td>
+                                <td>teste 1</td>
+                                <td>teste 1</td>
+                                <td>teste 1</td>
+                                <td>teste 1</td>
+                            </tr>
+                            <tr>
+                                <td>teste 1</td>
+                                <td>teste 1</td>
+                                <td>teste 1</td>
+                                <td>teste 1</td>
+                                <td>teste 1</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 @endsection
